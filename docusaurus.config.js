5504cf1ac736fffcd9560f572c9287bfc9f1191d
839781960 <114645865+839781960@users.noreply.github.com>
2023-03-14 12:42:06 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,8 +15,7 @@ const config = {
   url: 'https://839781960.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  //baseUrl: '/',------
-  baseUrl: '/ndn/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
